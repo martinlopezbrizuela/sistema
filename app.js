f97a66e8754bafff1be748b9c94509128cffe63a
martinlopezbrizuela <martinlopezbrizuela@gmail.com>
2026-05-16 22:48:54 -0300

    Sistema funcionando

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("KAVAJU ERP funcionando");
+
+document.querySelectorAll(".menu-item").forEach(btn => {
+
+  btn.addEventListener("click", () => {
+
+    document.querySelectorAll(".menu-item").forEach(b => {
+      b.classList.remove("active");
+    });
+
+    btn.classList.add("active");
+
+    const titulo = btn.textContent.trim();
+
+    document.querySelector(".topbar h1").textContent = titulo;
+
+    document.querySelector(".topbar p").textContent =
+      "Módulo " + titulo + " cargado correctamente";
+
+  });
+
+});
+
+document.querySelector(".new-btn").addEventListener("click", () => {
+
+  alert("Nuevo pedido próximamente.");
+
+});
